78b6f805e3f883177f3c7826081a501d26ad265f
david brooks <David.brooks1980@gmail.com>
2024-02-25 19:40:35 +0000

    all tests now pass

--- index.js
+++ index.js
@@ -14,7 +14,30 @@ const Employee = require("./lib/Employee");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-const employee1 = new Employee("Dave", 111, "dave@mail.com");
-const employee2 = new Employee("Cooper", 112, "cooper@mail.com");
-const employee3 = new Employee("Bob", 113, "bob@mail.com");
-
+const questions = [
+    {
+        type: 'input',
+        message: 'Please enter the team manager\'s name:',
+        name: 'manager'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the team manager\'s employee id:',
+        name: 'id'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the team manager\'s email address:',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'Please enter the team manager\'s office number:',
+        name: 'officeNumber'
+    },
+    {
+        type: 'option',
+        message: 'Please enter the team manager\'s name:',
+        name: ''
+    }
+]
