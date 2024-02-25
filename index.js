88a9df7b2a814dde112d5926685413d1f9a16536
david brooks <David.brooks1980@gmail.com>
2024-02-25 16:05:07 +0000

    added employee properties and methods

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+const Employee = require("./lib/Employee");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+const employee1 = new Employee("Dave", 111, "dave@mail.com");
+const employee2 = new Employee("Cooper", 112, "cooper@mail.com");
+const employee3 = new Employee("Bob", 113, "bob@mail.com");
 
